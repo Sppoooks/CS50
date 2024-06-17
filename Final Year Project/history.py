13cf4982dba10d16bd2a369840d8a2a94673718a
THOMAS WAN <thomas.wann14@gmail.com>
2024-06-17 15:34:03 +0200

    added view history finished main

--- Final Year Project/history.py
+++ Final Year Project/history.py
@@ -1,32 +1,58 @@
 import sqlite3
 
-def purchase(account_number, description, value):
-
-    #create or conenct purchase history database file
-    con1 = sqlite3.connect("purchase_history.db")
-    cur1 = con1.cursor()
-
+def get_username(account_number):
     #connect to user account database
-    con2 = sqlite3.connect("useraccounts.db")
-    cur2 = con2.cursor()
+    con = sqlite3.connect("useraccounts.db")
+    cur = con.cursor()
 
     #select which user to input purchase history
     sql_statement = f"SELECT First, Last FROM accounts WHERE User_ID = {account_number}"
-    res = cur2.execute(sql_statement)
+    res = cur.execute(sql_statement)
 
+    #format account username
     username = res.fetchall()[0]
     username = f"{username[0]} {username[1]}"
+    return username
+
+
+def save_purchase(account_number, description, value):
+
+    #create or connect purchase history database file
+    con = sqlite3.connect("purchase_history.db")
+    cur = con.cursor()
+
+    username = get_username(account_number)
 
     #create table for user if it doesn't exist
     sql_statement = f"CREATE TABLE IF NOT EXISTS '{username}' (Description text, Value integer)"
-    cur1.execute(sql_statement)
+    cur.execute(sql_statement)
 
+    #input description and value or purchase into db
     data = (description, value)
-    cur1.execute(f"INSERT INTO '{username}' (Description, Value) VALUES (?, ?)", data)
-    con1.commit()
+    cur.execute(f"INSERT INTO '{username}' (Description, Value) VALUES (?, ?)", data)
+    con.commit()
+    con.close()
 
+    print("Your purchase has been logged")
 
+def view_history(account_number):
+
+    #connect to purchase history
+    con = sqlite3.connect("purchase_history.db")
+    cur = con.cursor()
+
+    username = get_username(account_number)
+
+    #try to print all purchases registered
+    try:
+        sql_statement = f"SELECT * FROM '{username}'"
+        res = cur.execute(sql_statement)
+        for i in res.fetchall():
+            print(f"{i[0]}, {i[1]}")
+
+    except Exception as text:
+        print(f"{username} has made no purchases")
 
 
 if __name__ == "__main__":
-    purchase(1, "apple", 2000)
+    view_history(2)
--- Final Year Project/main.py
+++ Final Year Project/main.py
@@ -1,7 +1,71 @@
-#Personal finance app to keep track of spending
-#Includes bank account with balance, keeps history of purchase, withdraws and deposits
+import sqlite3
+import useraccount as user
+import history
 
-import user
+def main():
+    #Connect to database
+    con = sqlite3.connect("useraccounts.db")
+    cur = con.cursor()
+    _prompt = input("What would you like to do?\n1. Create account\n2. Choose from existing accounts\n")
 
-user = user.useraccount()
+    #Create new account or select existing account
+    match _prompt:
+        case "1":
+            user.useraccountsql()
+            sql_statement = f"SELECT * FROM accounts ORDER BY User_ID DESC LIMIT 1"
+            res = cur.execute(sql_statement)
+            account_number = int(res.fetchone()[0]) #Set account number to new account ID
 
+        case "2":
+            sql_statement = "SELECT User_ID, First, Last, Balance FROM accounts"
+            res = cur.execute(sql_statement)
+            for i in res.fetchall():
+                print(f"{i[0]}. {i[1]} {i[2]}, {i[3]}$")
+            #Select account ID
+            account_number = input("Select account number\n")
+
+    while True:
+        #Prompt user for action
+        _prompt = input("What would you like to do: \n1. Deposite \n2. Withdraw \n3. Make a Purchase\n4. View purchase history\n5. Exit\n")
+
+        #Update balance of user
+        match _prompt:
+            case "1":
+                amount = input("Amount: ")
+                sql_statement = f"UPDATE accounts SET Balance = Balance + {int(amount)} WHERE User_ID = {account_number}"
+                cur.execute(sql_statement)
+                con.commit()
+
+            case "2":
+                amount = input("Amount: ")
+                sql_statement = f"UPDATE accounts SET Balance = Balance - {int(amount)} WHERE User_ID = {account_number}"
+                cur.execute(sql_statement)
+                con.commit()
+
+            case "3":
+                #save purchase in db
+                description = input("What would you like to purchase?\n")
+                value = input("What is the cost of the purchase?\n")
+                history.save_purchase(account_number, description, value)
+
+                #update balance
+                sql_statement = f"UPDATE accounts SET Balance = Balance - {int(value)} WHERE User_ID = {account_number}"
+                cur.execute(sql_statement)
+                con.commit()
+
+            case "4":
+                #print purchase history of account user
+                history.view_history(account_number)
+
+            case "5":
+                #exit program
+                break
+
+        #Print new balance
+        sql_statement = f"SELECT Balance FROM accounts WHERE User_ID = {account_number}"
+        res = cur.execute(sql_statement)
+        print(f"Your balance is: {res.fetchone()[0]}\n")
+    con.close()
+
+if __name__ == "__main__":
+    main()
